59a3cef8c4b32072cd5a6afaed21ba7c3bcadbcb
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-02-15 15:05:44 -0600

    completed function and working for all instances

--- split_string_pair.js
+++ split_string_pair.js
@@ -1,24 +1,41 @@
 // PSEUDO CODE
 
-// CODE
-function solution(str){
-  var str = str.split(/.{2}/g);
-   if (str.length%2 != 0) {
 
-   }
+// CODE
+
+// REFACTOR
+function solution(str){
+  return (str.length%2 === 0 ? str : str + "_" ).match(/../g)
 }
+
+// ORIGINAL
+// function solution(str){
+//   var ar = str.match(/.{1,2}/g);
+//   if (str.length%2 !== 0) {
+//     ar[ar.length-1] += "_";
+//   }
+//   return ar;
+// }
+
 
 // DRIVER CODE
 solution('abc') // should return ['ab', 'c_']
 solution('abcdef') // should return ['ab', 'cd', 'ef']
 
-// TEST CASES WRITTEN
+
+
+
+
+// TEST CASES - Should be in a different file, just copying in here for ref for now.  
+
 // describe("Solution", function(){
 //   it("return pairs", function(){
-//     Test.assertEquals("actual", [ "ac", "tu", "al" ] );
-//     Test.assertEquals("asd023094-0", [ "as", "d0", "23", "09", "-0" ] );
-//   },
+//     let answerOne = solution("actual");
+//     let arr = ["ar", "tu", "al"];
+//     Test.expect( answerOne == arr );
+//     Test.expect(solution("asd023094-0") == ['as', 'd0', '23', '09', '-0'] )
+//   }); 
 //   it("returns an empty array if no string is provided", function() {
-//     Test.assertEquals( "", [], "expected empty array")
+//     Test.expect( solution("") == [], "expected empty array")
 //   });
 // });
